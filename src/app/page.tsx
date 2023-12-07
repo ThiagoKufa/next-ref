dc001d5d37a34236df2aaca30ccb2f023bf16ee5
Thiago Kufa <tkufa.code@gmail.com>
2023-12-07 13:43:48 -0300

    doc: update readme.md

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+
+import { useEffect } from "react";
+import { Test } from "@/components/Teste";
+
 export default function Home() {
+  useEffect(() => {
+    console.log("Ola mundo");
+  }, []);
   return (
     <main className="flex min-h-screen items-center justify-center">
       <h1 className="text-xl font-bold text-blue-500">Page</h1>
+      <Test />
     </main>
   );
 }
